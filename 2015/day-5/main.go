1d2d6b176b24b7e35dc4d67bfce46b5293856801
fabriciopk <bottegafabricio@gmail.com>
2021-06-09 22:03:57 -0300

    Day 5 part 2 solution

--- 2015/day-5/main.go
+++ 2015/day-5/main.go
@@ -22,27 +22,39 @@ func niceString(line string) bool {
 			return false
 		}
 	}
-
 	for _, r := range line {
 		if good_vowels[string(r)] {
 			good_vowels_cnt += 1
 		}
 	}
-
 	return nice_twice && good_vowels_cnt >= 3
+}
 
+func niceString2(line string) bool {
+	nice_pair := false
+	nice_overlap := false
+	for i := 0; i < len(line)-2; i++ {
+		if line[i] == line[i+2] {
+			nice_pair = true
+		}
+		if strings.Count(line, line[i:i+2]) >= 2 {
+			nice_overlap = true
+		}
+	}
+	return nice_pair && nice_overlap
 }
 
 func main() {
-	dat, _ := ioutil.ReadFile("/home/tib/workspace/adventOfCode/2015/day-5/input")
+	dat, _ := ioutil.ReadFile("input")
 	lines := strings.Split(string(dat), "\n")
 
 	sum_nice := 0
 	for _, line := range lines {
-		if niceString(line) {
+		if niceString2(line) {
 			sum_nice = sum_nice + 1
 		}
 	}
 
 	fmt.Println(sum_nice)
+
 }
